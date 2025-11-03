18f8fbd29adc806bcc0cb0d81dcd847534897977
Vaibhavalva <alva.vaibhav@gmail.com>
2025-11-03 08:36:57 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+package codingtask;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str= "Java";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println(rev);
+
+	}
+
+}
