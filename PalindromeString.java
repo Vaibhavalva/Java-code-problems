f7de7809afaab9345a3c58e0bc62c35d2770fcf1
Vaibhavalva <alva.vaibhav@gmail.com>
2025-11-04 18:18:58 +0530

    Add files via upload

---
+++ PalindromeString.java
@@ -0,0 +1,16 @@
+package codingtask;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		String str= "mam";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println(str.equals(rev));
+
+	}
+
+}
